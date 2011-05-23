59cb76174205e4f307f719f45d528f4d282ff2fa
Palmer Truelson <truelson@massivehealth.com>
2011-05-23 16:17:00 -0700

    Got rid of semicolons in FwMock

--- spec_helpers/FwMock.js
+++ spec_helpers/FwMock.js
@@ -6,131 +6,140 @@
  *  fireworks files 
  */
 // add underscore
-_ = require('../lib/underscore.js');
+_ = require('../lib/underscore.js')
 
 // it all starts with an object
-fw = {};
-exports.fw = fw;
+fw = {}
+exports.fw = fw
 
-fw._mockHelpers = {};
-fw.selection = [];
+fw._mockHelpers = {}
+fw.selection = []
 
 fw._mockHelpers.setBasics = function (obj) {
-  obj.height = 0;
-  obj.isLayer = false;
-  obj.left = 0;
-  obj.name = null;
-  obj.top = 0;
-  obj.width = 0;
-  obj.visible = true;
+  obj.height = 0
+  obj.isLayer = false
+  obj.left = 0
+  obj.name = null
+  obj.top = 0
+  obj.width = 0
+  obj.visible = true
+  obj.pathAttributes = {
+    brush: {
+      brushColor: '#FFFFFF',
+    },
+
+    fill: {
+      fillColor: '#FFFFFF',
+      name: 'Solid',
+      textureBlend: 0
+    }
+  }
 }
 
 // Group
 var Group = function (spec) {
-  fw._mockHelpers.setBasics(this);
+  fw._mockHelpers.setBasics(this)
   if(spec) {
-    _.extend(this, spec);
+    _.extend(this, spec)
   }
 }
 Group.prototype.toString = function () {
-  return '[object Group]';
+  return '[object Group]'
 }
-exports.Group = Group;
+exports.Group = Group
 
 // Image
 var Image = function (spec) {
-  fw._mockHelpers.setBasics(this);
+  fw._mockHelpers.setBasics(this)
   if(spec) {
-    _.extend(this, spec);
+    _.extend(this, spec)
   }
 }
 Image.prototype.toString = function () {
-  return '[object Image]';
+  return '[object Image]'
 }
-exports.Image = Image;
+exports.Image = Image
 
 // Instances
 var Instance = function (spec) {
-  fw._mockHelpers.setBasics(this);
-  this.transform = { matrix: [1, 0, 0, 0, 1, 0, 10, 20, 1] }; 
+  fw._mockHelpers.setBasics(this)
+  this.transform = { matrix: [1, 0, 0, 0, 1, 0, 10, 20, 1] } 
 
   if(spec) {
-    _.extend(this, spec);
+    _.extend(this, spec)
   }
 }
 Instance.prototype.toString = function () {
-  return '[object Instance]';
+  return '[object Instance]'
 }
-exports.Instance = Instance;
+exports.Instance = Instance
 
 var RectanglePrimitive = function (spec) {
-  fw._mockHelpers.setBasics(this);
-  if(spec) {
-    _.extend(this, spec);
-  }
+  fw._mockHelpers.setBasics(this)
+  if(spec) _.extend(this, spec) 
 }
 RectanglePrimitive.prototype.toString = function () {
-  return '[object RectanglePrimitive]';
+  return '[object RectanglePrimitive]'
 }
-exports.RectanglePrimitive = RectanglePrimitive;
+exports.RectanglePrimitive = RectanglePrimitive
 
 // Layer
 var Layer = function (spec) {
-  this.elems = []; 
-  this.isLayer = true; 
-  this.layerType = 'normal'; 
-  this.name = null;
-  this.visible = true;
+  this.elems = []
+  this.isLayer = true 
+  this.layerType = 'normal' 
+  this.name = null
+  this.visible = true
 
   if(spec) {
-    _.extend(this, spec);
+    _.extend(this, spec)
   }
-};
-Layer.prototype.toString = function () {
-  return '[object Layer]';
 }
-exports.Layer = Layer;
+Layer.prototype.toString = function () {
+  return '[object Layer]'
+}
+exports.Layer = Layer
 
 // Text
 var Text = function (spec) {
-  fw._mockHelpers.setBasics(this);
-  this.alignment = 'left';
+  fw._mockHelpers.setBasics(this)
+  this.alignment = 'left'
   this.pathAttributes = {
     fillColor: '#ff0000'
   },
-  this.font = '';
-  this.fontsize = 0;
-  this.name = null;
-  this.textChars = '';
+  this.font = ''
+  this.fontsize = 0
+  this.name = null
+  this.textChars = ''
 
   if(spec) {
-    _.extend(this, spec);
+    _.extend(this, spec)
   }
-};
-Text.prototype.toString = function () {
-  return '[object Text]';
 }
-exports.Text = Text;
+Text.prototype.toString = function () {
+  return '[object Text]'
+}
+exports.Text = Text
 
 fw._resetMockDom = function(d) {
-  d.pageName = 'TestView';
-  d.width = 320;
-  d.height = 480;
-  d.backgroundColor = '#ffffff';
+  d.pageName = 'TestView'
+  d.width = 320
+  d.height = 480
+  d.backgroundColor = '#ffffff'
   d.exportOptions = {
     colorMode: '32 bit'
   }
-  d.resolution = 300;
-  d.resolutionUnits = 1;
+  d.resolution = 300
+  d.resolutionUnits = 1
 
-  return d;
-};
+  return d
+}
 
 var mockLayer = {
   elems: [ ]
 }
 
-var mockCopyObj;
+var mockCopyObj
 
 fw.createDocument = function() {
   return fw._resetMockDom({
@@ -138,14 +147,14 @@ fw.createDocument = function() {
     exportElements: function (elems, file, imageDir, name) {},
 
     cloneSelection: function() {
-      mockCopyObj = fw.selection;
+      mockCopyObj = fw.selection
     },
     flattenSelection: function() {},
-    getSelectionBounds: function() {return {};},
+    getSelectionBounds: function() {return {}},
 
     clipCut: function() {},
     clipPaste: function() {
-      mockLayer.elems.push(mockCopyObj);
+      mockLayer.elems.push(mockCopyObj)
     },
 
     close: function() {},
@@ -155,37 +164,37 @@ fw.createDocument = function() {
     setDocumentCanvasSize: function() {},
 
     layers: [mockLayer]
-  });
-};
+  })
+}
 
-fw._mockDOM = fw.createDocument();
-fw.getDocumentDOM = function () {return fw._mockDOM;};
-fw.exportDocumentAs = function () {};
+fw._mockDOM = fw.createDocument()
+fw.getDocumentDOM = function () {return fw._mockDOM}
+fw.exportDocumentAs = function () {}
 
-fw.runScript = function (scriptName) {};
-fw.currentScriptDir = './';
+fw.runScript = function (scriptName) {}
+fw.currentScriptDir = './'
 
-fw.browseForFolderURL = function (name, hint) { return '~/TestDir'; };
+fw.browseForFolderURL = function (name, hint) { return '~/TestDir' }
 
 // Mocks of Fireworks class objects
 
 // File accessing mocks
-fw._mockFile = {};
-fw._mockFile.mockPath = '';
-fw._mockFile.write = function (text) {};
-fw._mockFile.writeUTF8 = function (text) {};
-fw._mockFile.close = function () {};
+fw._mockFile = {}
+fw._mockFile.mockPath = ''
+fw._mockFile.write = function (text) {}
+fw._mockFile.writeUTF8 = function (text) {}
+fw._mockFile.close = function () {}
 
-Files = {};
-exports.Files = Files;
+Files = {}
+exports.Files = Files
 
-Files.createFile = function (name, type, from) {};
-Files.createDirectory = function (name) {};
-Files.deleteFileIfExisting = function (name) {};
+Files.createFile = function (name, type, from) {}
+Files.createDirectory = function (name) {}
+Files.deleteFileIfExisting = function (name) {}
 
 Files.open = function (path, canWrite) {
-  fw._mockFile.mockPath = path;
-  fw._mockFile.canWrite = (canWrite || false);
-  return fw._mockFile;
-};
+  fw._mockFile.mockPath = path
+  fw._mockFile.canWrite = (canWrite || false)
+  return fw._mockFile
+}
 
